5dfcd212953b70dd5ba1ef492f68573c524877fe
tiktrimo <59413703+tiktrimo@users.noreply.github.com>
2020-06-07 15:35:20 +0900

    posted to web

--- src/component/Inbound.jsx
+++ src/component/Inbound.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import RoundHollowPipe from "./RoundHollowPipe";
 import LanguageSelection from "./LanguageSelection";
+import SquareHollowPipe from "./SquareHollowPipe";
 
 export default function Inbound(props) {
   const [languageCode, setLanguageCode] = useState(1);
@@ -10,6 +11,7 @@ export default function Inbound(props) {
     <Grid container justify="center">
       <LanguageSelection setLanguageCode={setLanguageCode} />
       <RoundHollowPipe languageCode={languageCode} />
+      <SquareHollowPipe languageCode={languageCode} />
     </Grid>
   );
 }
--- src/component/SquareHollowPipe.jsx
+++ src/component/SquareHollowPipe.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useCallback } from "react";
-import { Grid, Paper, TextField, InputAdornment } from "@material-ui/core";
-import hollowRoundPipe from "../svgs/HollowRoundPipe.svg";
+import React, { useCallback, useState } from "react";
+import { Grid, TextField, Paper, InputAdornment } from "@material-ui/core";
+import HollowSquarePipe from "../svgs/HollowSquarePipe.svg";
 
-export default function RoundHollowPipe(props) {
-  const [outerDiameter, setOuterDiameter] = useState(0);
+export default function SquareHollowPipe(props) {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
   const [wallThickness, setWallThickness] = useState(0);
 
-  const handleODchange = useCallback((e) => {
-    setOuterDiameter(Number(e.target.value));
+  const handleWidthChange = useCallback((e) => {
+    setWidth(e.target.value);
   }, []);
+  const handleHeightChange = useCallback((e) => {
+    setHeight(e.target.value);
+  });
   const handleWTchange = useCallback((e) => {
     setWallThickness(Number(e.target.value));
   }, []);
+
   return (
     <Paper
       style={{ width: "75%", padding: 20, margin: 5, maxWidth: 400 }}
@@ -21,23 +26,33 @@ export default function RoundHollowPipe(props) {
         <Grid container item justify="center" xs={12}>
           <img
             style={{ width: "80%" }}
-            src={hollowRoundPipe}
-            alt="Hollow Round Pipe"
+            src={HollowSquarePipe}
+            alt="Hollow Square Tube"
           />
         </Grid>
         <Grid container item justify="center" xs={12}>
           <TextField
-            id="RoundTube_OD"
-            label={props.languageCode === 1 ? "외경" : "Outer Diameter"}
+            id="SquareTube_Width"
+            label={props.languageCode === 1 ? "폭" : "Width"}
             InputProps={{
               endAdornment: <InputAdornment position="end">mm</InputAdornment>,
             }}
-            onChange={handleODchange}
+            onChange={handleWidthChange}
           />
         </Grid>
         <Grid container item justify="center" xs={12}>
           <TextField
-            id="RoundTube_T"
+            id="SquareTube_Height"
+            label={props.languageCode === 1 ? "높이" : "Height"}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">mm</InputAdornment>,
+            }}
+            onChange={handleHeightChange}
+          />
+        </Grid>
+        <Grid container item justify="center" xs={12}>
+          <TextField
+            id="SquareTube_T"
             label={props.languageCode === 1 ? "두께" : "Wall Thickness"}
             InputProps={{
               endAdornment: <InputAdornment position="end">mm</InputAdornment>,
@@ -48,16 +63,18 @@ export default function RoundHollowPipe(props) {
         <Grid container item justify="center" xs={12}>
           <TextField
             disabled
-            id="RoundTube_Section_Modulus"
+            id="SquareTube_Section_Modulus"
             label={props.languageCode === 1 ? "단면계수" : "Section Modulus"}
             InputProps={{
               endAdornment: <InputAdornment position="end">mm3</InputAdornment>,
             }}
             defaultValue={0}
             value={numberWithCommas(
-              calculateHollowTubeSectionModulus(
-                outerDiameter,
-                outerDiameter - 2 * wallThickness
+              calculateHollowSquareTubeSectionModulus(
+                width,
+                height,
+                width - wallThickness,
+                height - wallThickness
               )
             )}
           />
@@ -66,12 +83,17 @@ export default function RoundHollowPipe(props) {
     </Paper>
   );
 }
-function calculateHollowTubeSectionModulus(outerDiameter, innerDiameter) {
+function calculateHollowSquareTubeSectionModulus(
+  outerWidth,
+  outerHeight,
+  innerWidth,
+  innerHeight
+) {
   const sectionModulus =
-    (Math.PI * (Math.pow(outerDiameter, 4) - Math.pow(innerDiameter, 4))) /
-    (32 * outerDiameter);
+    (outerWidth * outerHeight * outerHeight) / 6 -
+    (innerWidth * Math.pow(innerHeight, 3)) / 6 / outerHeight;
 
-  return !Number.isNaN(sectionModulus) ? sectionModulus.toFixed(2) : undefined;
+  return !Number.isNaN(sectionModulus) ? sectionModulus.toFixed(2) : 0;
 }
 function numberWithCommas(x) {
   if (!x) return 0;
